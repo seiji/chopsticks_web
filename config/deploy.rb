d3681b405154e57890c5ce7659432280f018794c
Seiji Toyama <seijit@me.com>
2013-05-24 23:19:40 +0900

    Add ln nginx.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,15 @@
+require 'capistrano_colors'
+
+require "bundler/capistrano"
+
 set :application, "flot.in"
 set :repository, "git://github.com/seiji/flot.git"
+
 set :branch, fetch(:branch, "master")
 
 set :scm, "git"
 set :use_sudo, false
+
 set(:run_method) { use_sudo ? :sudo : :run }
 
 set :user, "deploy"
@@ -16,10 +22,11 @@ set :host, "#{user}@flot.in"
 role :web, host
 role :app, host
 
-set :rails_env, :production
 
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
+
+set :rack_env, :production
 
 set :deploy_to, "/var/www/#{application}"
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
@@ -27,13 +34,30 @@ set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
 # Unicorn control tasks
 namespace :deploy do
+
   task :restart do
-    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rack_env} -D; fi"
   end
+
   task :start do
     run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
   task :stop do
     run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#    run "mkdir -p #{shared_path}/config"
+    # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    # puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  # task :symlink_config, roles: :app do
+  #   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  # end
+  # after "deploy:finalize_update", "deploy:symlink_config"
+
+  
 end
