89abd8fba8c6e6180f5e264cd70c450f2d69b155
Seiji Toyama <seijit@me.com>
2013-05-25 15:20:20 +0900

    Add full host.

--- app/www.rb
+++ app/www.rb
@@ -1,6 +1,14 @@
 module Flot
   class WWW < Sinatra::Base
     enable :sessions, :logging
+
+    OmniAuth.config.full_host = lambda do |env|
+      scheme         = env['rack.url_scheme']
+      local_host     = env['HTTP_HOST']
+      forwarded_host = env['HTTP_X_FORWARDED_HOST']
+      forwarded_host.blank? ? "#{scheme}://#{local_host}" : "#{scheme}://#{forwarded_host}"
+    end
+    
     OMNIAUTH_YAML = File.join(settings.root, '..', 'private', 'config',  'omniauth.yml')
     OMNIAUTH_CONFIG = YAML.load_file(OMNIAUTH_YAML)["#{settings.environment}"]
 
