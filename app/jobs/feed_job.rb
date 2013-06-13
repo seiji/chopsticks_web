d49dcfec42cdd57395b0c498910bf47260b066f3
Seiji Toyama <seijit@me.com>
2013-06-13 21:46:01 +0900

    Eliminate boiler plate

--- app/jobs/feed_job.rb
+++ app/jobs/feed_job.rb
@@ -8,7 +8,8 @@ class FeedJob
 
   class << self
     def perform(feed_url, user_id)
-      zfeed = fetch feed_url
+
+      zfeed = Feedzirra::Feed.fetch_and_parse(feed_url)
       feed_title = zfeed.title
       feed_title.gsub!(/\n/, '')
       feed = Feed.where({feed_url: feed_url}).find_and_modify({ '$set'  => {
@@ -18,21 +19,8 @@ class FeedJob
                                                                 },
                                                                 '$inc'  => {subscriber_count: 1}},
                                                               {'upsert' => 'true', :new => true})
-      zfeed.sanitize_entries!
-      zfeed.entries.each do | zentry |
-        entry_title = zentry.title
-        entry_title.gsub!(/\n/, '')
-        entry = feed.entries.find_or_create_by(
-                                               url: zentry.url,
-                                               title: entry_title,
-                                               author: zentry.author,
-                                               summary: zentry.summary,
-                                               content: zentry.content,
-                                               )
-        entry.save
-      end
-      feed.save!
 
+      update_entries(feed, zfeed)
       if user_id
         user = User.find(user_id)
         if user and !user.feeds.where(feed_url: feed_url).first
@@ -54,6 +42,11 @@ class FeedJob
                              updated_at: Time.now
                              )
 
+      update_entries(feed, zfeed)
+    end
+
+    private
+    def update_entries(feed, zfeed)
       zfeed.sanitize_entries!
       zfeed.entries.each do | zentry |
         entry_title = zentry.title
@@ -68,18 +61,6 @@ class FeedJob
         entry.save
       end
       feed.save!
-      
-    end
-
-    def fetch(feed_url)
-      Feedzirra::Feed.fetch_and_parse(feed_url,
-                                      :on_success => lambda {|url, feed| # note that a return status of 304 (not updated) will call the on_success handler
-                                        
-                                      },
-                                      :on_failure => lambda {|url, response_code, responsea_header, response_body| # fail
-                                        
-                                      })
-
     end
   end
 end
