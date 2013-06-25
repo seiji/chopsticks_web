7d64db79b61e1c891068a02221ced1475c9943b6
Seiji Toyama <seijit@me.com>
2013-06-25 16:19:18 +0900

    Add notice bami2

--- app/jobs/feed_job.rb
+++ app/jobs/feed_job.rb
@@ -139,10 +139,6 @@ class FeedJob
       if %w(iphone android).any? {|word| /#{word}/i =~ feed.title or /#{word}/i =~ entry.title}
         session[:crashlogs].insert(message: message, _id:(Time.now.to_f * 1000.0).to_i)
       end
-
-      if %w(honwaka2ch.livedoor.biz).any?{|word| /#{word}/ =~ feed.url }
-        session[:bami2].insert(message: message, _id:(Time.now.to_f * 1000.0).to_i)
-      end
     end
   end
 end
